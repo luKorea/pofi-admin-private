5d4bc298f3ef1f5a688652a9fadcf675be6c01e6
korealu <643949593@qq.com>
2022-03-22 17:42:21 +0800

    fix(excel export): 新增导出Excel时loading效果
    新增导出Excel时loading效果

--- src/utils/exportExcel.ts
+++ src/utils/exportExcel.ts
@@ -1,13 +1,14 @@
 /*
  * @Author: korealu
  * @Date: 2022-02-22 14:38:21
- * @LastEditors: korealu
- * @LastEditTime: 2022-02-28 09:40:57
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-03-22 17:40:27
  * @Description: file content
  * @FilePath: /pofi-admin/src/utils/exportExcel.ts
  */
 // exportToExcel.ts
 import * as XLSX from 'xlsx'
+import { showLoading, hideLoading } from '@/utils'
 const EXCEL_TYPE =
   'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8'
 const EXCEL_EXTENSION = '.xlsx'
@@ -17,11 +18,13 @@ export class ExcelService {
     return `${excelFileName}-${new Date().getTime()}.xlsx`
   }
   public exportAsExcelFile(json: any[], excelFileName: string): void {
+    showLoading()
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json)
     const workbook: XLSX.WorkBook = {
       Sheets: { data: worksheet },
       SheetNames: ['data']
     }
     XLSX.writeFile(workbook, ExcelService.toExportFileName(excelFileName))
+    hideLoading()
   }
 }
