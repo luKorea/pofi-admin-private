1ecc9e3f865e4df1c54d96fdbb408f639a51edaa
korealu <643949593@qq.com>
2022-03-29 14:50:55 +0800

    feat(basic-ui/upload): 修改setupu语法在当前项目不支持bug
    修改setupu语法在当前项目不支持bug

--- src/base-ui/upload/index.ts
+++ src/base-ui/upload/index.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: korealu
  * @Date: 2022-02-14 14:07:22
- * @LastEditors: korealu
- * @LastEditTime: 2022-02-18 09:26:03
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-03-29 14:47:31
  * @Description: file content
  * @FilePath: /pofi-admin/src/base-ui/upload/index.ts
  */
--- src/base-ui/upload/src/upload.vue
+++ src/base-ui/upload/src/upload.vue
@@ -68,16 +68,8 @@
   </div>
 </template>
 
-<script lang="ts" setup>
-import {
-  ref,
-  computed,
-  watch,
-  toRefs,
-  onMounted,
-  defineProps,
-  defineEmits
-} from 'vue'
+<script lang="ts">
+import { ref, computed, watch, toRefs, onMounted, defineComponent } from 'vue'
 import Vuedraggable from 'vuedraggable'
 import { useOSSConfig } from '@/hooks/use-oss-config'
 import OSS from 'ali-oss'
@@ -89,271 +81,295 @@ import {
 } from '@/utils/index'
 import { errorTip, infoTipBox, warnTip } from '@/utils/tip-info'
 
-const props = defineProps({
-  fileTypeName: {
-    type: String,
-    default: 'help/'
+export default defineComponent({
+  props: {
+    fileTypeName: {
+      type: String,
+      default: 'help/'
+    },
+    value: {
+      type: Array,
+      default: () => []
+    },
+    otherOptions: {
+      type: Object,
+      default: () => ({})
+    },
+    // 限制上传图片的最大数量
+    limit: {
+      type: Number,
+      default: 0
+    },
+    // 限制数量文字是否展示
+    isShowLimit: {
+      type: Boolean,
+      default: true
+    },
+    // 限制单张图片的文件存储大小最大值(kb)
+    maxSize: {
+      type: Number,
+      default: 0
+    },
+    // 图片显示的宽度(px)
+    showWidth: {
+      type: Number,
+      default: 126
+    },
+    // 图片显示的高度(px)
+    showHeight: {
+      type: Number,
+      default: 126
+    },
+    // 是否禁用
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    // 限定图片分辨率宽度值(px)
+    screenWidth: {
+      type: Number,
+      default: 0
+    },
+    // 限定图片分辨率高度值(px)
+    screenHeight: {
+      type: Number,
+      default: 0
+    },
+    // 限制图片分辨率宽度最大值(px)
+    maxScreenWidth: {
+      type: Number,
+      default: 0
+    },
+    // 限制图片分辨率高度最大值(px)
+    maxScreenHeight: {
+      type: Number,
+      default: 0
+    }
   },
-  value: {
-    type: Array,
-    default: () => []
+  components: {
+    Vuedraggable
   },
-  otherOptions: {
-    type: Object,
-    default: () => ({})
-  },
-  // 限制上传图片的最大数量
-  limit: {
-    type: Number,
-    default: 0
-  },
-  // 限制数量文字是否展示
-  isShowLimit: {
-    type: Boolean,
-    default: true
-  },
-  // 限制单张图片的文件存储大小最大值(kb)
-  maxSize: {
-    type: Number,
-    default: 0
-  },
-  // 图片显示的宽度(px)
-  showWidth: {
-    type: Number,
-    default: 126
-  },
-  // 图片显示的高度(px)
-  showHeight: {
-    type: Number,
-    default: 126
-  },
-  // 是否禁用
-  disabled: {
-    type: Boolean,
-    default: false
-  },
-  // 限定图片分辨率宽度值(px)
-  screenWidth: {
-    type: Number,
-    default: 0
-  },
-  // 限定图片分辨率高度值(px)
-  screenHeight: {
-    type: Number,
-    default: 0
-  },
-  // 限制图片分辨率宽度最大值(px)
-  maxScreenWidth: {
-    type: Number,
-    default: 0
-  },
-  // 限制图片分辨率高度最大值(px)
-  maxScreenHeight: {
-    type: Number,
-    default: 0
-  }
-})
-const emit = defineEmits({
-  'update:value': null
-})
-
-let client = ref<any>()
-const isUploading = ref(false) // 正在上传状态
-const isFirstMount = ref(true) // 控制防止重复回显
-
-const uploadRef = ref<any>(null)
-
-// 图片数组数据
-const imgList = computed({
-  get(): any {
-    // 此处代码目的为判断文件类型，显示不同组件
-    if (props.value && props.value.length > 0) {
-      props.value.forEach((item: any) => {
-        let name = item.name
-        if (item.name.indexOf('?')) {
-          name = item.name.split('?')[0]
+  emits: ['update:value'],
+  setup(props, { emit }) {
+    let client = ref<any>()
+    const isUploading = ref(false) // 正在上传状态
+    const isFirstMount = ref(true) // 控制防止重复回显
+    const uploadRef = ref<any>(null)
+    // 图片数组数据
+    const imgList = computed({
+      get(): any {
+        // 此处代码目的为判断文件类型，显示不同组件
+        if (props.value && props.value.length > 0) {
+          props.value.forEach((item: any) => {
+            let name = item.name
+            if (item.name.indexOf('?')) {
+              name = item.name.split('?')[0]
+            }
+            item['type'] = fileTypeIsImage(name)
+          })
         }
-        item['type'] = fileTypeIsImage(name)
-      })
-    }
-    return props.value
-  },
-  set(val: any) {
-    emit('update:value', val)
-  }
-})
-// 是否是单图上传
-const isSingle = computed(() => {
-  return +props.limit === 1
-})
-
-// watch监听
-const { value } = toRefs(props)
-watch(
-  value,
-  () => {
-    if (!isFirstMount.value) {
-      return
-    }
-    // 异步数据的回显
-    if (props.value.length > 0) {
-      syncElUpload()
-    }
-  },
-  { deep: true }
-)
-
-// mounted事件
-onMounted(() => {
-  // 同步数据的回显
-  if (props.value.length > 0) {
-    syncElUpload()
-  }
-})
-
-// 同步el-upload数据
-function syncElUpload(val?: any) {
-  const list: any = val || imgList.value
-  if (uploadRef.value) {
-    uploadRef.value.uploadFiles = list.map((v: any, i: number) => {
-      return {
-        name: v.url,
-        url: v.url,
-        percentage: 100,
-        status: 'success',
-        uid: createUniqueString()
+        return props.value
+      },
+      set(val: any) {
+        emit('update:value', val)
       }
     })
-  }
-  isFirstMount.value = false
-}
+    // 是否是单图上传
+    const isSingle = computed(() => {
+      return +props.limit === 1
+    })
+    watch(
+      props.value,
+      () => {
+        if (!isFirstMount.value) {
+          return
+        }
+        // 异步数据的回显
+        if (props.value.length > 0) {
+          syncElUpload()
+        }
+      },
+      { deep: true }
+    )
 
-// 上传图片之前
-function beforeUpload(file: any) {
-  isFirstMount.value = false
-  isUploading.value = true
-  return new Promise((resolve, reject) => {
-    useOSSConfig()
-      .then((res) => {
-        client.value = new OSS({
-          region: 'oss-cn-hongkong',
-          stsToken: res.securityToken,
-          bucket: res.bucketName,
-          ...res
+    // mounted事件
+    onMounted(() => {
+      // 同步数据的回显
+      if (props.value.length > 0) {
+        syncElUpload()
+      }
+    })
+
+    // 同步el-upload数据
+    function syncElUpload(val?: any) {
+      const list: any = val || imgList.value
+      if (uploadRef.value) {
+        uploadRef.value.uploadFiles = list.map((v: any, i: number) => {
+          return {
+            name: v.url,
+            url: v.url,
+            percentage: 100,
+            status: 'success',
+            uid: createUniqueString()
+          }
         })
-        resolve(client.value)
-      })
-      .catch((err) => reject(err))
-  })
-}
-const httpRequest = (options: any) => {
-  async function uploadImage() {
-    if (client.value !== null) {
+      }
       isFirstMount.value = false
-      const file = options.file
-      const suffix = '.' + file.type.split('/')[1]
-      const name = props.fileTypeName + client.value.options.fileName + suffix
-      client.value.multipartUpload(name, file).then((res: any) => {
-        const url = `${OSSURL}/${res.name}`
-        imgList.value.push({
-          name: res.name + '?' + new Date().getTime(),
-          url: url + '?' + new Date().getTime()
-        })
-        isUploading.value = false
+    }
+
+    // 上传图片之前
+    function beforeUpload(file: any) {
+      isFirstMount.value = false
+      isUploading.value = true
+      return new Promise((resolve, reject) => {
+        useOSSConfig()
+          .then((res) => {
+            client.value = new OSS({
+              region: 'oss-cn-hongkong',
+              stsToken: res.securityToken,
+              bucket: res.bucketName,
+              ...res
+            })
+            resolve(client.value)
+          })
+          .catch((err) => reject(err))
       })
     }
-  }
-  uploadImage()
-}
-
-// 上传完单张图片
-function onSuccess(res: any, file: any) {
-  isUploading.value = false
-}
-
-// 上传出错
-function onError(err: any, file: any) {
-  errorTip(file.name + ' 上传失败')
-  isUploading.value = false
-  console.log(err)
-}
-
-// 移除单张图片
-function onRemoveHandler(index: number) {
-  infoTipBox({
-    title: '提示',
-    message: '确定删除该图片?'
-  }).then(() => {
-    imgList.value.splice(index, 1)
-    syncElUpload()
-  })
-}
-
-// 超限
-function onExceed() {
-  // uploadRef.value.abort() // 取消剩余接口请求
-  syncElUpload()
-  warnTip(`图片超限，最多可上传${props.limit}张图片`)
-}
-
-function onDragStart(e: any) {
-  e.target.classList.add('hideShadow')
-}
-function onDragEnd(e: any) {
-  e.target.classList.remove('hideShadow')
-}
-
-function getItemKey() {
-  return createUniqueString()
-}
-
-// 校验图片占用大小
-function validateSize(file: any) {
-  console.log(file)
-  const size = +props.maxSize
-  const isSizeOut = file.size / 1024 > size
-  if (isSizeOut) {
-    errorTip(`上传图片大小不能超过${formatStorage(size * 1024)}`)
-    return false
-  } else {
-    isUploading.value = true
-    return true
-  }
-}
-// 校验上传图片分辨率
-function validateImgScreen(file: any) {
-  return new Promise((resolve, reject) => {
-    const fileReader = new FileReader()
-    fileReader.onload = (e: any) => {
-      const base64Url = e.target.result
-      const img = new Image()
-      img.onload = () => {
-        // console.log(img.width, img.height)
-        if (props.screenWidth && +img.width !== +props.screenWidth) {
-          reject()
-          errorTip(`上传图片宽度限制只能为${props.screenWidth}像素`)
-        } else if (props.screenHeight && +img.height !== +props.screenHeight) {
-          reject()
-          errorTip(`上传图片高度限制只能为${props.screenHeight}像素`)
-        } else if (props.maxScreenWidth && +img.width > +props.maxScreenWidth) {
-          reject()
-          errorTip(`上传图片宽度不能超过${props.maxScreenWidth}像素`)
-        } else if (
-          props.maxScreenHeight &&
-          +img.height > +props.maxScreenHeight
-        ) {
-          reject()
-          errorTip(`上传图片高度不能超过${props.maxScreenHeight}像素`)
-        } else {
-          isUploading.value = true
-          resolve(true)
+    const httpRequest = (options: any) => {
+      async function uploadImage() {
+        if (client.value !== null) {
+          isFirstMount.value = false
+          const file = options.file
+          const suffix = '.' + file.type.split('/')[1]
+          const name =
+            props.fileTypeName + client.value.options.fileName + suffix
+          client.value.multipartUpload(name, file).then((res: any) => {
+            const url = `${OSSURL}/${res.name}`
+            imgList.value.push({
+              name: res.name + '?' + new Date().getTime(),
+              url: url + '?' + new Date().getTime()
+            })
+            isUploading.value = false
+          })
         }
       }
-      img.src = base64Url
+      uploadImage()
     }
-    fileReader.readAsDataURL(file)
-  })
-}
+
+    // 上传完单张图片
+    function onSuccess(res: any, file: any) {
+      isUploading.value = false
+    }
+
+    // 上传出错
+    function onError(err: any, file: any) {
+      errorTip(file.name + ' 上传失败')
+      isUploading.value = false
+      console.log(err)
+    }
+
+    // 移除单张图片
+    function onRemoveHandler(index: number) {
+      infoTipBox({
+        title: '提示',
+        message: '确定删除该图片?'
+      }).then(() => {
+        imgList.value.splice(index, 1)
+        syncElUpload()
+      })
+    }
+
+    // 超限
+    function onExceed() {
+      // uploadRef.value.abort() // 取消剩余接口请求
+      syncElUpload()
+      warnTip(`图片超限，最多可上传${props.limit}张图片`)
+    }
+
+    function onDragStart(e: any) {
+      e.target.classList.add('hideShadow')
+    }
+    function onDragEnd(e: any) {
+      e.target.classList.remove('hideShadow')
+    }
+
+    function getItemKey() {
+      return createUniqueString()
+    }
+
+    // 校验图片占用大小
+    function validateSize(file: any) {
+      console.log(file)
+      const size = +props.maxSize
+      const isSizeOut = file.size / 1024 > size
+      if (isSizeOut) {
+        errorTip(`上传图片大小不能超过${formatStorage(size * 1024)}`)
+        return false
+      } else {
+        isUploading.value = true
+        return true
+      }
+    }
+    // 校验上传图片分辨率
+    function validateImgScreen(file: any) {
+      return new Promise((resolve, reject) => {
+        const fileReader = new FileReader()
+        fileReader.onload = (e: any) => {
+          const base64Url = e.target.result
+          const img = new Image()
+          img.onload = () => {
+            // console.log(img.width, img.height)
+            if (props.screenWidth && +img.width !== +props.screenWidth) {
+              reject()
+              errorTip(`上传图片宽度限制只能为${props.screenWidth}像素`)
+            } else if (
+              props.screenHeight &&
+              +img.height !== +props.screenHeight
+            ) {
+              reject()
+              errorTip(`上传图片高度限制只能为${props.screenHeight}像素`)
+            } else if (
+              props.maxScreenWidth &&
+              +img.width > +props.maxScreenWidth
+            ) {
+              reject()
+              errorTip(`上传图片宽度不能超过${props.maxScreenWidth}像素`)
+            } else if (
+              props.maxScreenHeight &&
+              +img.height > +props.maxScreenHeight
+            ) {
+              reject()
+              errorTip(`上传图片高度不能超过${props.maxScreenHeight}像素`)
+            } else {
+              isUploading.value = true
+              resolve(true)
+            }
+          }
+          img.src = base64Url
+        }
+        fileReader.readAsDataURL(file)
+      })
+    }
+    return {
+      isSingle,
+      isUploading,
+      isFirstMount,
+      uploadRef,
+      imgList,
+      validateImgScreen,
+      validateSize,
+      getItemKey,
+      onDragEnd,
+      onDragStart,
+      onExceed,
+      onSuccess,
+      onError,
+      beforeUpload,
+      httpRequest,
+      onRemoveHandler
+    }
+  }
+})
 </script>
 
 <style lang="less" scoped>
