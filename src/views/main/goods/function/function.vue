eb570ba65e09b2bd2aa3697d7cfab0fd43b7661b
korealu <643949593@qq.com>
2022-04-29 16:46:10 +0800

    fix(goods/good): 修改资源商品中功能套餐
    修改资源商品中功能套餐

--- src/views/main/goods/function/function.vue
+++ src/views/main/goods/function/function.vue
@@ -2,7 +2,7 @@
  * @Author: korealu
  * @Date: 2022-02-16 16:58:51
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-04-29 15:30:09
+ * @LastEditTime: 2022-04-29 16:43:17
  * @Description: file content
  * @FilePath: /pofi-admin/src/views/main/finance/tradeRecord/tradeRecord.vue
 -->
@@ -45,12 +45,39 @@
       :otherInfo="otherInfo"
       @changeSelect="handleChangeSelect"
     >
+      <el-row>
+        <el-col :span="24">
+          <div class="item-flex">
+            <span class="item-title">资源名称</span>
+            <el-select
+              placeholder="请输入资源名称"
+              style="width: 100%"
+              filterable
+              remote
+              reserve-keyword
+              clearable
+              multiple
+              :remote-method="getResourceList"
+              :loading="loading"
+              v-model="otherInfo.moId"
+            >
+              <el-option
+                v-for="option in resourceList"
+                :key="option.moId"
+                :value="option.moId"
+                :label="option.pname"
+                >{{ option.pname }}</el-option
+              >
+            </el-select>
+          </div>
+        </el-col>
+      </el-row>
     </page-modal>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed, nextTick } from 'vue'
+import { defineComponent, ref, computed, nextTick, watchEffect } from 'vue'
 
 import { searchFormConfig } from './config/search.config'
 import { contentTableConfig } from './config/content.config'
@@ -62,8 +89,10 @@ import { usePageModal } from '@/hooks/use-page-modal'
 import { useStoreName } from './hooks/use-page-list'
 import { ExcelService } from '@/utils/exportExcel'
 import { unityModalList } from '@/utils/select-list/map-resource-list'
-import { mapResourceInfo } from '../../../../service/main/goods/function'
+import { mapResourceInfo } from '@/service/main/goods/function'
 import { errorTip } from '@/utils/tip-info'
+import { getCommonSelectList1 } from '@/service/common'
+import { _debounce } from '@/utils'
 
 export default defineComponent({
   name: 'goodsFunction',
@@ -89,28 +118,62 @@ export default defineComponent({
       const ExportExcel = new ExcelService()
       ExportExcel.exportAsExcelFile(result.value, '功能套餐')
     }
+
+    const loading = ref<boolean>(false)
+    const resourceList = ref<any>([])
+    const getResourceList = _debounce(
+      (resource: string) => {
+        loading.value = true
+        getCommonSelectList1(resource, false)
+          .then((res) => {
+            if (res.result == 0) {
+              resourceList.value = res.data
+            } else errorTip(res.msg)
+          })
+          .finally(() => (loading.value = false))
+      },
+      300,
+      true
+    )
     // 编辑新增操作
-    const otherInfo = ref<any>()
+    const otherInfo = ref<any>({
+      moId: []
+    })
+    const newData = () => {
+      otherInfo.value = {
+        moId: []
+      }
+    }
     const editData = (item: any) => {
-      mapResourceName(+item.type)
-      setTimeout(() => {
-        let moId = null
-        if (item.moId && item.moId !== undefined) {
-          moId = item.moId.split(',')
-        } else moId = []
-        item.PNId = 'PN' + item.id
-        item.modeType = item.type
+      // mapResourceName(+item.type)
+      if (item.moId && item.moId !== undefined) {
         otherInfo.value = {
-          id: item.id
+          moId: item.moId.split(',')
         }
-        handleEditData({
-          ...item,
-          moId
-        })
-      }, 500)
+        for (let i = 0; i < item.moId.split(',').length; i++) {
+          getCommonSelectList1(item.moId.split(',')[i], false).then((res) => {
+            if (res.result == 0) {
+              resourceList.value = res.data
+            } else errorTip(res.msg)
+          })
+        }
+      } else {
+        otherInfo.value = {
+          moId: []
+        }
+      }
+      item.PNId = 'PN' + item.id
+      item.modeType = item.type
+      otherInfo.value = {
+        ...otherInfo.value,
+        id: item.id
+      }
+      handleEditData({
+        ...item
+      })
     }
     const [pageModalRef, defaultInfo, handleNewData, handleEditData] =
-      usePageModal()
+      usePageModal(newData)
     const formatDeveloped = (type: number | string) => {
       switch (+type) {
         case 1:
@@ -170,7 +233,9 @@ export default defineComponent({
       formatDeveloped,
       otherInfo,
       handleChangeSelect,
-      editData
+      editData,
+      getResourceList,
+      resourceList
     }
   }
 })
--- src/views/main/goods/good/good.vue
+++ src/views/main/goods/good/good.vue
@@ -2,7 +2,7 @@
  * @Author: korealu
  * @Date: 2022-02-16 16:58:51
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-04-29 15:48:35
+ * @LastEditTime: 2022-04-29 15:57:50
  * @Description: file content
  * @FilePath: /pofi-admin/src/views/main/finance/tradeRecord/tradeRecord.vue
 -->
