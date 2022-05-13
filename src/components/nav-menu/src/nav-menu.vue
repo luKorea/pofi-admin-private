86a6048754f551eef0775b92d123ebd189e68d59
korealu <643949593@qq.com>
2022-05-13 09:43:37 +0800

    fix(menu): 修改正式服菜单颜色
    修改正式服菜单颜色

--- src/components/nav-menu/src/nav-menu.vue
+++ src/components/nav-menu/src/nav-menu.vue
@@ -2,7 +2,7 @@
  * @Author: korealu 643949593@qq.com
  * @Date: 2022-04-20 09:33:38
  * @LastEditors: korealu 643949593@qq.com
- * @LastEditTime: 2022-05-12 18:25:15
+ * @LastEditTime: 2022-05-13 09:42:34
  * @FilePath: /pofi-admin-private/src/components/nav-menu/src/nav-menu.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -12,7 +12,7 @@
     :style="{ backgroundColor: currentEnv === 4 ? '#88001F' : '#001529' }"
   >
     <el-menu
-      :default-active="defaultValue"
+      :default-active="currentEnv !== 4 && defaultValue"
       class="el-menu-vertical"
       :collapse="collapse"
       :background-color="currentEnv === 4 ? '#A40026' : '#0c2135'"
