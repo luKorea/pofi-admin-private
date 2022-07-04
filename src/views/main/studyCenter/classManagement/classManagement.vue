08ac98ad62e4e51680cc5611ec898221866deacf
korealu <643949593@qq.com>
2022-07-04 16:28:55 +0800

    feat(studycenter): 新增自学中心路由文件夹
    新增自学中心路由文件夹

---
+++ src/views/main/studyCenter/classManagement/classManagement.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: korealu 643949593@qq.com
+ * @Date: 2022-07-04 16:22:38
+ * @LastEditors: korealu 643949593@qq.com
+ * @LastEditTime: 2022-07-04 16:23:05
+ * @FilePath: /pofi-admin-private/src/views/main/studyCenter/classManagement/classManagement.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div class="class-management">
+    <h2>classManagementComponent</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'classManagementComponent',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
